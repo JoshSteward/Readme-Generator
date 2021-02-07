5dfd93d0d85e5aee5043a2db1713c56fd1eb374d
Josh Steward <stewardjosh10@gmail.com>
2021-02-07 11:52:44 +1100

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generate = require('./utils/generateMarkdown');
 
 // * Your application should prompt the user for information like their name, location, bio, LinkedIn URL, and GitHub URL. Feel free to add any additional prompts you think of.
 
@@ -8,22 +9,22 @@ inquirer
     {
       type: 'input',
       message: 'What is your project title?',
-      name: 'Title',
+      name: 'title',
     },
     {
       type: 'input',
-      message: 'Where are you located?',
-      name: 'location',
+      message: 'Please input your badge links',
+      name: 'badge',
     },
     {
       type: 'input',
       message: 'Write your description',
-      name: 'Description',
+      name: 'description',
     },
     {
       type: 'input',
       message: 'Please provide installation steps',
-      name: 'Installation',
+      name: 'installation',
     },
     {
       type: 'input',
@@ -32,45 +33,33 @@ inquirer
     },
     {
       type: 'input',
+      message: 'Please provide license',
+      name: 'license',
+    },
+    {
+      type: 'input',
       message: 'Please provide your github username',
       name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Please provide contributers',
+        name: 'contributers',
     },
     {
       type: 'input',
       message: 'Please provide a link to your github profile',
       name: 'githublink',
-    }
+    },
+    {
+      type: 'input',
+      message: 'Please provide repo link',
+      name: 'repository',
+    },
     
   ])
-  .then((response) => {
-    let data = `
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <style>
-                p {
-                    font-size: 20px;
-                    font-weight: bold;
-                }
-            </style>
-        </head>
-        <body>
-            <div>
-                <p>
-                    Name: ${response.name}<br>
-                    Location: ${response.location}<br>
-                    Bio: ${response.bio}<br>
-                    LinkedIn URL: ${response.linkedin}<br>
-                    GitHub URL: ${response.github}
-                </p>
-            </div>
-        </body>
-    <html>
-    `
+  
     fs.writeFile('index.html', data, (err) =>
         err ? console.error(err) : console.log('Success!')
-    );
-    }
-  );
-
+    ); 
   
---
+++ utils/GenerateMarkdown.js
@@ -0,0 +1,48 @@
+function generateMarkdown(data, githubInfo){ 
+    return `
+    #**${data.title}**
+
+    ${data.badge}
+
+    ##Descritption 
+
+    ${data.description}
+
+    ##Table of Contents 
+
+    - [Description](#Description)
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Licence](#Licence)
+    - [Contributors](#Contributors)
+    - [Test](#Test)
+    - [Repository Link](#Repository)
+    - [GitHub Info](#GitHub) 
+
+    ##Installation
+
+    ${data.installation}
+
+    ##Usage 
+
+    ${data.usage}
+
+    ##License 
+
+    ${data.license}
+
+    ##Contributers 
+
+    ${data.contributers}
+
+    ##Repository 
+
+    ${data.repository}
+
+    #Github
+
+    ${data.githubLink}
+    `
+}
+
+module.exports = generateMarkdown;
